327d1091bebded566ccf1fa07395751be711b2fd
Damian Fratesi <dfratesi@users.noreply.github.com>
2020-04-10 19:45:49 -0300

    Actualizar funciones.sql
    Se cambio en obtener_ventas el nombre de la variable @paginas a @ventas, más acorde con el uso de la función.

--- 10.-transacciones/funciones.sql
+++ 10.-transacciones/funciones.sql
@@ -10,8 +10,8 @@ END//
 CREATE FUNCTION obtener_ventas()
 RETURNS INT
 BEGIN
-  SET @paginas = (SELECT (ROUND( RAND() * 100 ) * 6 ));
-  RETURN @paginas;
+  SET @ventas = (SELECT (ROUND( RAND() * 100 ) * 6 ));
+  RETURN @ventas;
 END//
 
 DELIMITER ;
